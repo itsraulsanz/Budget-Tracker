8b8df0bfa6329cfa046d5c586bc0801b6c290ae4
Raul Sanz <luar_znas@hotmail.com>
2021-07-11 15:41:17 +0100

    Deployment

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,48 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-
-const urls = [
+const cacheURLs = [
   "/",
-  "/index.html",  
+  "/index.html",
   "/manifest.json",
+  "/styles.css",
   "/db.js",
   "/index.js",
-  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
-// install
+// INSTALL Service Worker
 self.addEventListener("install", function (evt) {
+  // pre cache static elements (URLs))
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      cache.addAll(urls);
-    })
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.addAll(cacheURLs))
   );
+  // Activate service worker after installation
   self.skipWaiting();
 });
 
-// activate
+// ACTIVATE Service Worker - cleaning up old caches
 self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-      caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheName !== CACHE_NAME && cacheName !== DATA_CACHE_NAME) {
-              console.log("Remove old cache data", cacheName);
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
+  evt.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME && cacheName !== DATA_CACHE_NAME) {
+            console.log("Remove old cache data", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 
-// fetch
+  self.clients.claim();
+});
+
+// RESPONSE / FETCH of Service Worker
+
 self.addEventListener("fetch", function (evt) {
-  // cache all get requests to /api routes
+  // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches
@@ -51,32 +50,28 @@ self.addEventListener("fetch", function (evt) {
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
-              // If the response was good, clone it and store it in the cache.
+              // If the response = good, clone + cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
               return response;
             })
             .catch((err) => {
-              // Network request failed, try to get it from the cache.
+              // If Network request fails, get data from the cache.
               return cache.match(evt.request);
             });
         })
         .catch((err) => console.log(err))
     );
+
     return;
   }
+  // Event reponds with static cache
   evt.respondWith(
-    fetch(evt.request).catch(function () {
-      return caches.match(evt.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
       });
     })
   );
 });
-
